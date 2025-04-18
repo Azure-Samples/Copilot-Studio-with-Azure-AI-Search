599a0f917920a259af03eaa6172f20012e6536cd
Ian Jensen <94021216+ianjensenisme@users.noreply.github.com>
2025-04-18 10:56:49 -0700

    Initial Code Migration (#30)
    * Fixing a typo
    
    * Initial migration attempt
    
    * Update .gitignore
    
    Co-authored-by: Matt Dotson <mattdot@users.noreply.github.com>
    
    * Updates per review comments
    
    ---------
    
    Co-authored-by: Matt Dotson <mattdot@users.noreply.github.com>

---
+++ infra/main.ai.tf
@@ -0,0 +1,19 @@
+module "azure_open_ai" {
+  source                = "Azure/avm-res-cognitiveservices-account/azurerm"
+  version               = "0.6.0"
+  kind                  = "OpenAI"
+  location              = var.location
+  name                  = "aoai${random_string.name.id}"
+  resource_group_name   = azurerm_resource_group.this.name
+  enable_telemetry      = true
+  sku_name              = "S0"
+  local_auth_enabled    = true
+  cognitive_deployments = var.cognitive_deployments
+  network_acls = {
+    default_action = "Deny"
+  }
+  managed_identities = {
+    system_assigned = true
+  }
+  tags = var.tags
+}
