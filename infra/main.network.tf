2d192a8a9784e570019d64be288fd2541a52444d
Eduardo Sanchez <edsanche@microsoft.com>
2025-05-14 10:28:28 -0600

    fix: remove unused dependency on virtual networks and update powerplatform version to 3.7.0

--- infra/main.network.tf
+++ infra/main.network.tf
@@ -89,7 +89,6 @@ resource "azurerm_nat_gateway" "nat_gateways" {
   resource_group_name = azurerm_resource_group.this.name
   sku_name            = "Standard"
 
-#  depends_on = [module.primary_virtual_network, module.failover_virtual_network]
 
 }
 
