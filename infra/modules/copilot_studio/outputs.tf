b626182e2288d23162c37f15d38a8f621b320de0
Eduardo Sanchez <eduardodfmex@hotmail.com>
2025-05-15 00:50:28 -0600

    Eduardo/62 bug cant delete azd deployment subnet is in use by the nat gw (#69)
    * fix(nat_gateway): ensure proper dependency on virtual networks
    
    * fix: update NAT gateway dependency and adjust powerplatform version
    
    * fix(outputs): correct description punctuation for power platform environment ID
    
    * fix: remove unused dependency on virtual networks and update powerplatform version to 3.7.0

--- infra/modules/copilot_studio/outputs.tf
+++ infra/modules/copilot_studio/outputs.tf
@@ -1,7 +1,8 @@
 output "power_platform_environment_id" {
-  description = "The ID of the Power Platform environment"
+  description = "The ID of the Power Platform environment."
   value       = local.power_platform_environment_id
 }
+
 
 # required for AVM interface
 output "resource_id" {
