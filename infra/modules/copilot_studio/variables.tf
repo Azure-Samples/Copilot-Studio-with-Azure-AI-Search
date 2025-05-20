f1c6dfb5c7b76aee8ff2ed950bc5f1708107409b
Eduardo Sanchez <eduardodfmex@hotmail.com>
2025-05-20 11:10:48 -0600

    64 bug fix tflint warnings (#73)
    * fix: remove unnecessary string interpolation for variables in application insights workbook
    
    * refactor: simplify string interpolation in application insights and network configurations
    chore: remove unused variables from the variables file
    
    * fix: add missing type declarations for app insights variables
    
    * refactor: comment out unused variable declaration for resource name
    
    * docs: update README with authentication instructions for User Account and Service Principal
    
    * fix(provider): enable MSI and Azure AD storage usage for azurerm provider, Fixing 403 Key Based Authentication Error on Azure Storage Account.
    
    * refactor: remove unused variables and clean up formatting in main.tf and variables.tf

--- infra/modules/copilot_studio/variables.tf
+++ infra/modules/copilot_studio/variables.tf
@@ -8,10 +8,7 @@ variable "failover_vnet_name" {
   description = "The name of the failover Virtual Network to be used in connecting Power Platform to Azure. Note that failover networks are required for Power Platform's enterprise policy connection."
 }
 
-variable "name" {
-  type        = string
-  description = "The name of this resource."
-}
+
 
 variable "power_platform_environment" {
   type = object({
@@ -98,21 +95,6 @@ A map describing customer-managed keys to associate with the resource. This incl
 - `user_assigned_identity` - (Optional) An object representing a user-assigned identity with the following properties:
   - `resource_id` - The resource ID of the user-assigned identity.
 DESCRIPTION  
-}
-
-variable "disable_copilot_with_bing" {
-  type        = bool
-  default     = false
-  description = <<DESCRIPTION
-TODO
-DESCRIPTION
-}
-
-
-variable "power_platform_api_version" {
-  type        = string
-  default     = "2023-11-01"
-  description = "The version of the Power Platform API to use"
 }
 
 # List of connections to manage using the module.
