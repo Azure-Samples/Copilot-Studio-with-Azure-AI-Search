8d24a279300b7adf409cc74dee6d4fb38be45d60
Hadwa Gaber <hadwa.gaber@hotmail.com>
2025-05-22 11:37:08 -0700

    enhance scripts

--- azd-hooks/scripts/hooks/preprovision/run_gitleaks.ps1
+++ azd-hooks/scripts/hooks/preprovision/run_gitleaks.ps1
@@ -1,5 +1,29 @@
+function Check-Gitleaks {
+    Write-Host "Checking if Gitleaks is installed...`n"
+
+    # Check if Gitleaks exists in system path
+    $gitleaksPath = Get-Command gitleaks -ErrorAction SilentlyContinue
+
+    if ($null -eq $gitleaksPath) {
+        Write-Error "Gitleaks is not installed or not found in PATH!`n"
+        exit 1
+    } else {
+        Write-Host "Gitleaks is installed at: $($gitleaksPath.Source)`n"
+        # Ensure /usr/local/bin is in PATH
+        $env:PATH += ":/usr/local/bin"
+        # Check if Gitleaks is executable
+        if (-not (Test-Path $gitleaksPath.Source)) {
+            Write-Error "Gitleaks is not executable!`n"
+            exit 1
+        } else {
+            Write-Host "Gitleaks is executable.`n"
+        }
+
+    }
+}
+
 function Run-Gitleaks {
-     param (
+    param (
         [string]$ReportFormat,
         [string]$LogLevel,
         [switch]$Redact,
@@ -7,14 +31,14 @@ function Run-Gitleaks {
         [switch]$NoGit
     )
 
-    write-Host "Run Gitleaks detect cmd...`n"
+    Write-Host "Run Gitleaks detect cmd...`n"
 
     $SourcePath = (Get-Location)
 
     # Construct command options as an array
     $cmdOptions = @(
         "detect"
-        "--config", "$SourcePath\.gitleaks.toml"
+        "--config", "$SourcePath/.gitleaks.toml"
         "--source", "$SourcePath"
         "--report-path", "./gitleaks-report.$ReportFormat"
         "--report-format", "$ReportFormat"
@@ -30,7 +54,6 @@ function Run-Gitleaks {
     gitleaks @cmdOptions
     $exitCode = $LASTEXITCODE
 
-    # return $exitCode
     if ($exitCode -ne 0) {
         Write-Error "gitleaks failed`n"
         exit 1
@@ -42,4 +65,8 @@ function Run-Gitleaks {
 
 }
 
+# First, check if Gitleaks is installed
+Check-Gitleaks
+
+# Then, run Gitleaks scan
 Run-Gitleaks -ReportFormat "sarif" -LogLevel "info" -Redact -Verbose
