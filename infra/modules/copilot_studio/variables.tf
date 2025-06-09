757cb584424cbcc8a0e51ca933fee9feec99a723
Eduardo Sanchez <edsanche@microsoft.com>
2025-06-09 14:12:23 -0600

    feat: update Power Platform environment user security role to include Bot Author

--- infra/modules/copilot_studio/variables.tf
+++ infra/modules/copilot_studio/variables.tf
@@ -115,5 +115,5 @@ variable "tags" {
 variable "pp_environment_user_Security_role" {
   description = "Power Platform environment user security role, examples of roles can be: System Administrator, System Customizer, basic user and many more."
   type        = list(string)
-  default     = ["Basic User", "System Administrator"]
+  default     = ["Basic User","Bot Author"]
 }
