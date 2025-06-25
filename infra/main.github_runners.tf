eab93d944d36598fc8fb85a228db0957ad32fdc0
Eduardo Sanchez <eduardodfmex@hotmail.com>
2025-06-25 10:28:17 -0600

    fix: standardize formatting and improve readability in Terraform configuration files (#144)

--- infra/main.github_runners.tf
+++ infra/main.github_runners.tf
@@ -4,15 +4,15 @@ module "github_runner_aca_primary" {
   count  = var.deploy_github_runner ? 1 : 0
   source = "./modules/github_runner_aca"
 
-  environment_name            = var.azd_environment_name
-  unique_id                   = random_string.name.id
-  location                    = var.primary_location
-  resource_group_name         = azurerm_resource_group.this.name
-  infrastructure_subnet_id    = azurerm_subnet.github_runner_primary_subnet[0].id
-  private_endpoint_subnet_id  = azurerm_subnet.pe_primary_subnet.id
-  virtual_network_id          = azurerm_virtual_network.primary_virtual_network.id
-  github_runner_config        = var.github_runner_config
-  openai_endpoint             = module.azure_open_ai.endpoint
+  environment_name           = var.azd_environment_name
+  unique_id                  = random_string.name.id
+  location                   = var.primary_location
+  resource_group_name        = azurerm_resource_group.this.name
+  infrastructure_subnet_id   = azurerm_subnet.github_runner_primary_subnet[0].id
+  private_endpoint_subnet_id = azurerm_subnet.pe_primary_subnet.id
+  virtual_network_id         = azurerm_virtual_network.primary_virtual_network.id
+  github_runner_config       = var.github_runner_config
+  openai_endpoint            = module.azure_open_ai.endpoint
 
   tags = merge(var.tags, local.env_tags)
 }
@@ -45,15 +45,15 @@ module "github_runner_aca_failover" {
   count  = var.deploy_github_runner && var.enable_failover_github_runner ? 1 : 0
   source = "./modules/github_runner_aca"
 
-  environment_name            = "${var.azd_environment_name}-failover"
-  unique_id                   = "${random_string.name.id}-fo"
-  location                    = var.failover_location
-  resource_group_name         = azurerm_resource_group.this.name
-  infrastructure_subnet_id    = azurerm_subnet.github_runner_failover_subnet[0].id
-  private_endpoint_subnet_id  = azurerm_subnet.pe_failover_subnet.id
-  virtual_network_id          = azurerm_virtual_network.failover_virtual_network.id
-  github_runner_config        = var.github_runner_config
-  openai_endpoint             = module.azure_open_ai.endpoint
+  environment_name           = "${var.azd_environment_name}-failover"
+  unique_id                  = "${random_string.name.id}-fo"
+  location                   = var.failover_location
+  resource_group_name        = azurerm_resource_group.this.name
+  infrastructure_subnet_id   = azurerm_subnet.github_runner_failover_subnet[0].id
+  private_endpoint_subnet_id = azurerm_subnet.pe_failover_subnet.id
+  virtual_network_id         = azurerm_virtual_network.failover_virtual_network.id
+  github_runner_config       = var.github_runner_config
+  openai_endpoint            = module.azure_open_ai.endpoint
 
   tags = merge(var.tags, local.env_tags)
 }
