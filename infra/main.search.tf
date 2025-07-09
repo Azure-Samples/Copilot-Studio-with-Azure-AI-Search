20a83a8a7d302d6056f148b5a581229deea1d1c5
mawasile <50197777+mawasile@users.noreply.github.com>
2025-07-09 10:15:30 +0000

    refactor: move Checkov skip comment to resource declaration for clarity

--- infra/main.search.tf
+++ infra/main.search.tf
@@ -3,8 +3,7 @@ locals {
 }
 
 # checkov:skip=CKV_AZURE_209: Deploying with minimal infrastructure for evaluation. Update partition_count and replica_count for production scenarios.
-# checkov:skip=CKV_AZURE_208: Deploying with minimal infrastructure for evaluation. Update partition_count and replica_count for production scenarios.
-resource "azurerm_search_service" "ai_search" {
+resource "azurerm_search_service" "ai_search" { # checkov:skip=CKV_AZURE_208: Deploying with minimal infrastructure for evaluation. Update partition_count and replica_count for production scenarios.
   name                          = local.search_name
   location                      = var.primary_location
   resource_group_name           = azurerm_resource_group.this.name
