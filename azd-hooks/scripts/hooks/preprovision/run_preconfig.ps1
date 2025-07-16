e1ff629dfe04f4c757b1295b0bd4395e0b7e0722
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2025-07-16 11:44:50 +0000

    fix: add use_azuread_auth to remote storage configuration for improved authentication

--- azd-hooks/scripts/hooks/preprovision/run_preconfig.ps1
+++ azd-hooks/scripts/hooks/preprovision/run_preconfig.ps1
@@ -84,8 +84,9 @@ function Initialize-RemoteStorage {
 {
     "storage_account_name": "`${RS_STORAGE_ACCOUNT}`",
     "container_name": "`${RS_CONTAINER_NAME}`",
-    "key": "azd/${AZURE_ENV_NAME}/terraform.tfstate",
-    "resource_group_name": "`${RS_RESOURCE_GROUP}`"
+    "key": "azd/`${AZURE_ENV_NAME}`/terraform.tfstate",
+    "resource_group_name": "`${RS_RESOURCE_GROUP}`",
+    "use_azuread_auth": "true"
 }
 "@
         Set-Content -Path $script:providerConfPath -Value $remoteStorageConfig -Encoding UTF8
