d431a58efd39789dbdb93e3b4b7cddf5d1d7d2c1
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2025-08-04 08:57:23 +0000

    fix: update GitHub runner installation script to use correct token variable and display additional repo information

--- cicd/github_runner_vm/install-github-runner2.sh
+++ cicd/github_runner_vm/install-github-runner2.sh
@@ -8,11 +8,18 @@ set -e
 # Variables passed from Terraform
 RUNNER_NAME="${runner_name}"
 GITHUB_URL="${github_url}"
-GITHUB_TOKEN="${github_token}"
+GITHUB_RUNNER_TOKEN="${github_runner_token}"
+REPO_NAME="${repo_name}"
+REPO_OWNER="${repo_owner}"
 
 echo "Runner Name: '$RUNNER_NAME'"
 echo "Github URL: '$GITHUB_URL'"
-echo "Github Token: '$GITHUB_TOKEN'"
+echo "Github Runner Token: '$GITHUB_RUNNER_TOKEN'"
+echo "Repo Name: '$REPO_NAME'"
+echo "Repo Owner: '$REPO_OWNER'"
+
+
+echo "GITHUB_TOKEN1 environment variable: '${GITHUB_TOKEN}'"
 
 # Create a folder
 mkdir actions-runner && cd actions-runner # Download the latest runner package
@@ -20,5 +27,5 @@ curl -o actions-runner-linux-x64-2.327.1.tar.gz -L https://github.com/actions/ru
 echo "d68ac1f500b747d1271d9e52661c408d56cffd226974f68b7dc813e30b9e0575  actions-runner-linux-x64-2.327.1.tar.gz" | shasum -a 256 -c # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.327.1.tar.gz
 
-sudo RUNNER_ALLOW_RUNASROOT=true ./config.sh --url $GITHUB_URL --token $GITHUB_TOKEN
+sudo RUNNER_ALLOW_RUNASROOT=true ./config.sh --url $GITHUB_URL --token $GITHUB_RUNNER_TOKEN
 ./run.sh
