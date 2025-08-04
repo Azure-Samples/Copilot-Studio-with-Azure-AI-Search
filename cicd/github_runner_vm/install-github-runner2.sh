9ba6baa3f6a5410a3a177e2728011ee71daa8629
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2025-08-04 08:36:32 +0000

    fix: update echo statements to include runner name and ensure GITHUB_URL is used correctly

--- cicd/github_runner_vm/install-github-runner2.sh
+++ cicd/github_runner_vm/install-github-runner2.sh
@@ -6,10 +6,13 @@
 set -e
 
 # Variables passed from Terraform
+RUNNER_NAME="${runner_name}"
 GITHUB_URL="${github_url}"
 GITHUB_TOKEN="${github_token}"
 
-echo "Github URL: $GITHUB_URL"
+echo "Runner Name: '$RUNNER_NAME'"
+echo "Github URL: '$GITHUB_URL'"
+echo "Github Token: '$GITHUB_TOKEN'"
 
 # Create a folder
 mkdir actions-runner && cd actions-runner # Download the latest runner package
@@ -17,5 +20,5 @@ curl -o actions-runner-linux-x64-2.327.1.tar.gz -L https://github.com/actions/ru
 echo "d68ac1f500b747d1271d9e52661c408d56cffd226974f68b7dc813e30b9e0575  actions-runner-linux-x64-2.327.1.tar.gz" | shasum -a 256 -c # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.327.1.tar.gz
 
-sudo RUNNER_ALLOW_RUNASROOT=true ./config.sh --url https://github.com/Azure-Samples/Copilot-Studio-with-Azure-AI-Search --token $GITHUB_TOKEN
+sudo RUNNER_ALLOW_RUNASROOT=true ./config.sh --url $GITHUB_URL --token $GITHUB_TOKEN
 ./run.sh
