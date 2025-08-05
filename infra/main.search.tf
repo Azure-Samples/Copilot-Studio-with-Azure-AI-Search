db04d86bee64df7ea273cf4ca5d148e349b63bd2
Sergii Baidachnyi <sbaydach@microsoft.com>
2025-08-04 17:41:04 -0700

    shorter names

--- infra/main.search.tf
+++ infra/main.search.tf
@@ -25,14 +25,14 @@ resource "azurerm_search_service" "ai_search" {
 # Primary region private endpoint
 resource "azurerm_private_endpoint" "primary_endpoint" {
   location            = azurerm_search_service.ai_search.location
-  name                = "private-endpoint-primary-${azurecaf_name.main_names.results["azurerm_search_service"]}"
+  name                = "pe-primary-${azurecaf_name.main_names.results["azurerm_search_service"]}"
   resource_group_name = local.resource_group_name
   subnet_id           = local.pe_primary_subnet_id
   tags                = var.tags
 
   private_service_connection {
     is_manual_connection           = false
-    name                           = "private-connection-primary-${azurecaf_name.main_names.results["azurerm_search_service"]}"
+    name                           = "pc-primary-${azurecaf_name.main_names.results["azurerm_search_service"]}"
     private_connection_resource_id = azurerm_search_service.ai_search.id
     subresource_names              = ["searchService"]
   }
@@ -43,14 +43,14 @@ resource "azurerm_private_endpoint" "primary_endpoint" {
 # Failover region private endpoint
 resource "azurerm_private_endpoint" "failover_endpoint" {
   location            = local.failover_virtual_network_location
-  name                = "private-endpoint-failover-${azurecaf_name.main_names.results["azurerm_search_service"]}"
+  name                = "pe-failover-${azurecaf_name.main_names.results["azurerm_search_service"]}"
   resource_group_name = local.resource_group_name
   subnet_id           = local.pe_failover_subnet_id
   tags                = var.tags
 
   private_service_connection {
     is_manual_connection           = false
-    name                           = "private-connection-failover-${azurecaf_name.main_names.results["azurerm_search_service"]}"
+    name                           = "pc-failover-${azurecaf_name.main_names.results["azurerm_search_service"]}"
     private_connection_resource_id = azurerm_search_service.ai_search.id
     subresource_names              = ["searchService"]
   }
