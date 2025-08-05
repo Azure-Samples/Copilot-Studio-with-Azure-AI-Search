50cf998caeaded06b80ec609856f09291b35c539
Sergii Baidachnyi <sbaydach@microsoft.com>
2025-08-05 08:01:55 -0700

    moving naming into a separate file

--- infra/main.tf
+++ infra/main.tf
@@ -8,10 +8,6 @@ locals {
   use_existing_resource_group = var.resource_group_name != null && length(var.resource_group_name) > 0
   resource_group_name         = local.use_existing_resource_group ? var.resource_group_name : azurerm_resource_group.this[0].name
   resource_group_location     = local.use_existing_resource_group ? data.azurerm_resource_group.existing[0].location : var.location
-
-  # Organization suffixes and prefixes are optional, and we need to form an array of non-empty values only
-  org_prefix = compact([var.org_naming.org_prefix])
-  org_suffix = compact([var.org_naming.org_environment, var.org_naming.org_suffix])
 }
 
 
@@ -19,84 +15,6 @@ locals {
 data "azurerm_resource_group" "existing" {
   count = local.use_existing_resource_group ? 1 : 0
   name  = var.resource_group_name
-}
-
-# Generate unique names for primary resources
-resource "azurecaf_name" "main_names" {
-  name = var.org_naming.workload_name
-  resource_types = [
-    "azurerm_resource_group",
-    "azurerm_storage_account",
-    "azurerm_search_service",
-    "azurerm_cognitive_account",
-    "azurerm_virtual_network",
-    "azurerm_network_security_group",
-    "azurerm_virtual_network_gateway",
-    "azurerm_public_ip"
-  ]
-  prefixes      = local.org_prefix
-  suffixes      = local.org_suffix
-  random_length = 4
-  # use_slug = false
-  clean_input = true
-}
-
-# Generate unique names for failover resources
-resource "azurecaf_name" "failover_names" {
-  name = var.org_naming.workload_name
-  resource_types = [
-    "azurerm_virtual_network",
-    "azurerm_network_security_group",
-    "azurerm_virtual_network_gateway",
-    "azurerm_public_ip"
-  ]
-  prefixes      = local.org_prefix
-  suffixes      = concat(local.org_suffix, ["failover"])
-  random_length = 4
-  # use_slug = false
-  clean_input = true
-}
-
-# Generate unique names for primary private endpoint subnet
-resource "azurecaf_name" "main_pe_subnet_names" {
-  name = var.org_naming.workload_name
-  resource_types = [
-    "azurerm_subnet"
-  ]
-  prefixes      = concat(["pe"], local.org_prefix)
-  suffixes      = concat(local.org_suffix, ["primary"])
-  random_length = 4
-  # use_slug = false
-  clean_input = true
-}
-
-# Generate unique names for failover private endpoint subnet
-resource "azurecaf_name" "failover_pe_subnet_names" {
-  name = var.org_naming.workload_name
-  resource_types = [
-    "azurerm_subnet"
-  ]
-  prefixes      = concat(["pe"], local.org_prefix)
-  suffixes      = concat(local.org_suffix, ["failover"])
-  random_length = 4
-  # use_slug = false
-  clean_input = true
-}
-
-# Generate unique names for Azure Deployment Script related resources
-resource "azurecaf_name" "deployment_script_names" {
-  name = var.org_naming.workload_name
-  resource_types = [
-    "azurerm_storage_account",
-    "azurerm_network_security_group",
-    "azurerm_subnet",
-    "azurerm_user_assigned_identity"
-  ]
-  prefixes      = local.org_prefix
-  suffixes      = concat(local.org_suffix, ["script"])
-  random_length = 4
-  # use_slug = false
-  clean_input = true
 }
 
 # The unique ID that will be included in most resources managed by this module
