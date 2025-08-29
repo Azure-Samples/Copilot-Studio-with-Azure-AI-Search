c3865cbc287978877ce6e7cdc3769b29e7bf4ef6
Matt Dotson <mattdot@users.noreply.github.com>
2025-08-29 05:42:46 +0000

    refactor: remove unused variables and commented code from runner configuration

--- cicd/variables.tf
+++ cicd/variables.tf
@@ -88,23 +88,6 @@ variable "github_pat" {
   default     = ""
 }
 
-# variable "github_runner_vm_size" {
-#   type        = string
-#   default     = "Standard_D2s_v3"
-#   description = "VM size for the GitHub runner"
-# }
-
-# variable "github_runner_os_type" {
-#   type        = string
-#   default     = "linux"
-#   description = "Operating system type for the GitHub runner VM."
-#   validation {
-#     condition     = contains(["linux"], var.github_runner_os_type)
-#     error_message = "OS type must be 'linux'. Other OS types are not supported yet"
-#   }
-# }
-
-
 variable "network_config" {
   type = object({
     vnet_address_space                  = list(string)
@@ -164,40 +147,3 @@ variable "network_config" {
     error_message = "All subnets must have prefix lengths greater than or equal to their containing VNet. Verify manually that subnets are properly contained within VNet address spaces."
   }
 }
-
-# variable "github_runner_config" {
-#   type = object({
-#     image_name                 = string
-#     image_tag                  = string
-#     github_pat                 = string
-#     github_repo_owner          = string
-#     github_repo_name           = string
-#     github_runner_group        = string
-#     github_runner_image_branch = string
-#     min_replicas               = number
-#     max_replicas               = number
-#     cpu_requests               = string
-#     memory_requests            = string
-#     workload_profile_type      = string
-#   })
-#   description = "Configuration for GitHub self-hosted runners"
-#   sensitive   = true
-# }
-
-# variable "primary_gh_runner_subnet_address_spaces" {
-#   type        = list(string)
-#   default     = ["10.1.10.0/23"]
-#   description = "GitHub runner subnet address spaces in the primary VNET. Ensure there are no collisions with existing subnets. Must be /23 or larger for Container App Environment."
-# }
-
-# variable "failover_gh_runner_subnet_address_spaces" {
-#   type        = list(string)
-#   default     = ["10.2.10.0/23"]
-#   description = "GitHub runner subnet address spaces in the failover VNET. Must be /23 or larger for Container App Environment."
-# }
-
-# variable "enable_failover_github_runner" {
-#   type        = bool
-#   default     = false # Disabled to reduce runtime
-#   description = "Enable the GitHub Actions self-hosted runner in the failover region. Set to true to deploy failover runner resources."
-# }
