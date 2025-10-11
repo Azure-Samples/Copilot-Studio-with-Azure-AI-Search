c2403d5f31b191980f4289dca50665752353e9db
Matt Dotson <mattdot@users.noreply.github.com>
2025-10-11 06:25:52 +0000

    fix: correct formatting of local variables in main.naming.tf

--- infra/main.naming.tf
+++ infra/main.naming.tf
@@ -2,8 +2,8 @@
 
 locals {
   # Organization suffixes and prefixes are optional, and we need to form an array of non-empty values only
-  org_prefix = compact([var.org_naming.org_prefix])
-  org_suffix = compact([var.org_naming.org_environment, var.org_naming.org_suffix])
+  org_prefix   = compact([var.org_naming.org_prefix])
+  org_suffix   = compact([var.org_naming.org_environment, var.org_naming.org_suffix])
   azd_env_seed = parseint(substr(md5(var.azd_environment_name), 0, 8), 16)
 }
 
@@ -24,7 +24,7 @@ resource "azurecaf_name" "main_names" {
   prefixes      = local.org_prefix
   suffixes      = local.org_suffix
   random_length = 4
-  random_seed = local.azd_env_seed
+  random_seed   = local.azd_env_seed
   # use_slug = false
   clean_input = true
 }
